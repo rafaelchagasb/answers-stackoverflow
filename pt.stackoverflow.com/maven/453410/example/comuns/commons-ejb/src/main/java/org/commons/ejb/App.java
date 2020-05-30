5343b4f586e0c56fca4b9b4d1ce45669545db46b
Rafael Chagas <rafa15chagas@gmail.com>
2020-05-30 11:22:55 -0300

    add project with modules

---
+++ pt.stackoverflow.com/maven/453410/example/comuns/commons-ejb/src/main/java/org/commons/ejb/App.java
@@ -0,0 +1,15 @@
+package org.commons.ejb;
+
+import org.commons.Utils;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( Utils.upperCase("texto"));
+    }
+}
---
+++ pt.stackoverflow.com/maven/453410/example/comuns/commons/src/main/java/org/commons/Utils.java
@@ -0,0 +1,9 @@
+package org.commons;
+
+public class Utils {
+
+	public static String upperCase(String text) {
+		return text.toUpperCase();
+	}
+	
+}
